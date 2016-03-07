171998c4c182ae021d45b0c83013a570b8107506
Steve Domino <domino.steve@gmail.com>
2016-03-07 14:52:42 -0700

    updating build script to use correct md5 lib per OS

--- build.sh
+++ build.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
+# try and use the correct MD5 lib (depending on user OS darwin/linux)
+MD5=$(which md5 || echo "$(which md5sum) | cut -f 1" )`
+
 # remove any previous builds that may have failed
 [ -e "./build" ] && \
   echo "Cleaning up old builds..." && \
   rm -rf "./build"
 
-# build hoader
-echo "Building HOARDER and uploading it to 's3://tools.nanopack.io/hoader'"
-gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./build/{{.OS}}/{{.Arch}}/hoader"
+# build hoarder
+echo "Building HOARDER and uploading it to 's3://tools.nanopack.io/hoarder'"
+gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./build/{{.OS}}/{{.Arch}}/hoarder"
 
 # look through each os/arch/file and generate an md5 for each
 echo "Generating md5s..."
 for os in $(ls ./build); do
   for arch in $(ls ./build/${os}); do
     for file in $(ls ./build/${os}/${arch}); do
-      cat "./build/${os}/${arch}/${file}" | md5 >> "./build/${os}/${arch}/${file}.md5"
+      cat "./build/${os}/${arch}/${file}" | ${MD5} >> "./build/${os}/${arch}/${file}.md5"
     done
   done
 done
 
 # upload to AWS S3
 echo "Uploading builds to S3..."
-aws s3 sync ./build/ s3://tools.nanopack.io/hoader --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
+aws s3 sync ./build/ s3://tools.nanopack.io/hoarder --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
 
 #
 echo "Cleaning up..."
