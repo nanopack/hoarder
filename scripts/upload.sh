c7f13144556194fa0b9a97616c89ff5e50c68c2c
Steve Domino <domino.steve@gmail.com>
2016-06-03 14:52:50 -0600

    breaking build and upload out into scripts folder

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,15 +4,6 @@ set -e
 # try and use the correct MD5 lib (depending on user OS darwin/linux)
 MD5=$(which md5 || which md5sum )
 
-# remove any previous builds that may have failed
-[ -e "./build" ] && \
-  echo "Cleaning up old builds..." && \
-  rm -rf "./build"
-
-# build hoarder
-echo "Building HOARDER and uploading it to 's3://tools.nanopack.io/hoarder'"
-gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./build/{{.OS}}/{{.Arch}}/hoarder"
-
 # look through each os/arch/file and generate an md5 for each
 echo "Generating md5s..."
 for os in $(ls ./build); do
@@ -26,11 +17,3 @@ done
 # upload to AWS S3
 echo "Uploading builds to S3..."
 aws s3 sync ./build/ s3://tools.nanopack.io/hoarder --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
-
-#
-echo "Cleaning up..."
-
-# remove build
-[ -e "./build" ] && \
-  echo "Removing build files..." && \
-  rm -rf "./build"
